14cd6291b6bfa9a5a2a466337138de776bca98cb
rileyevans <riley_evans@asl.org>
2014-11-11 15:00:12 +0000

    i put in a code

--- Lab05.java
+++ Lab05.java
@@ -2,8 +2,8 @@
 *
 * Description of the program goes here  // provide a brief description
 *
-* @author <Your Name here>  // replace <...> with your name
-* @version <date/of/completion> // replace <...> with the date
+* @author <Riley Evans>
+* @version <Nov. 11, 2014>
 */
 
 import edu.fcps.karel2.Display;
@@ -12,6 +12,28 @@ import edu.fcps.karel2.Robot;
 public class Lab05 {
 
      public static void main(String[] args) {
-          // TODO Your code goes here
-     }
+     Display.openWorld("maps/shuttle.map");
+     Display.setSize(10,10);
+     Display.setSpeed(15);
+      
+      Racer spence = new Racer(1);
+      Racer dana = new Racer(4);
+     Racer rob = new Racer(7);
+      
+     spence.shuttle(2,8); 
+      spence.shuttle(4,6);
+      spence.shuttle(6,4); 
+      spence.move();
+      
+      dana.shuttle(2,8); 
+      dana.shuttle(4,6);
+      dana.shuttle(6,4);    
+      dana.move();
+      
+      rob.shuttle(2,8); 
+      rob.shuttle(4,6);
+      rob.shuttle(6,4);
+      rob.move();
+
+    }
 }
